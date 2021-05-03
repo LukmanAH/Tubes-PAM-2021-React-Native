23709d8b0b9f52c46e1d8aa09926c785a44c029c
Muhammad Lukman Al Hakim <56007339+Lukboy@users.noreply.github.com>
2021-05-04 05:00:46 +0700

    create Navigator and CRUD

--- Favorit.js
+++ Favorit.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import { style } from './Style'
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native'
+import Home from './Home';
 
+const Favorit = ({ navigation }) => {
 
+  const goToHome = () => {
+    navigation.navigate('home')
+  }
 
-class Favorit extends Component {
+  return (
+    <View style={{ flexDirection: 'column', flex: 1 }}>
+      <View style={styles.navView}>
+
+        <TouchableOpacity
+          style={styles.buttonNavigation}
+          onPress={goToHome}
+        >
+          <Text>Back</Text>
+        </TouchableOpacity>
+
+        <View style={styles.titlePageView}>
+          <text>Favorite Character</text>
+        </View>
+
+      </View>
+      <ListFavorite />
+    </View >
+  );
+}
+
+class ListFavorite extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -31,33 +57,6 @@ class Favorit extends Component {
   }
 
 
-  tambahin() {
-    this.setState({ nama: pokemon.name });
-    this.klikSimpan();
-
-  }
-
-  klikSimpan() {
-    if (this.state.nama == '') {
-      alert('Silakan masukkan nama');
-    } else {
-      var urlAksi = this.url + "/?op=create";
-
-      fetch(urlAksi, {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: "nama=" + this.state.nama
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          this.setState({ nama: '' });
-          this.ambilListData();
-        })
-    }
-  }
-
   async klikDelete(id) {
     await fetch(this.url + "/?op=delete&id=" + id)
       .then((response) => response.json())
@@ -70,34 +69,20 @@ class Favorit extends Component {
       })
   }
 
+
+
   render() {
     return (
-      <View style={style.viewWrapper}>
-        <View style={style.viewData}>
+      <View style={styles.viewWrapper}>
+        <View style={styles.viewData}>
           {
             this.state.listData.map((val, index) => (
-              <View style={style.viewList} key={index}>
-                <Text style={style.textListNama}>{val.nama}</Text>
-                <Text style={style.textListDelete} onPress={() => this.klikDelete(val.id)}>Delete</Text>
+              <View style={styles.viewList} key={index}>
+                <Text style={styles.textListNama}>{val.nama}</Text>
+                <Text style={styles.textListDelete} onPress={() => this.klikDelete(val.id)}>Delete</Text>
               </View>
             ))
           }
-        </View>
-
-        <View style={style.viewForm}>
-          <TextInput
-            style={style.textInput}
-            placeholder="Masukkan Nama"
-            value={this.state.nama}
-            onChangeText={(text) => this.setState({ nama: text })}
-          >
-          </TextInput>
-
-          <Button
-            title="Masukkan Data"
-            onPress={() => this.klikSimpan()}>
-
-          </Button>
         </View>
       </View>
     );
@@ -105,3 +90,81 @@ class Favorit extends Component {
 }
 
 export default Favorit;
+
+
+const styles = StyleSheet.create({
+  viewWrapper: {
+    flex: 11,
+    backgroundColor: 'red'
+  },
+  viewForm: {
+    flex: 2,
+    padding: 10
+  },
+  viewData: {
+    flex: 4
+  },
+  textInput: {
+    padding: 10,
+    fontSize: 15,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    marginBottom: 10,
+    backgroundColor: '#dedede'
+  },
+  viewList: {
+    flexDirection: 'row',
+    padding: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#dedede'
+  },
+  textListNama: {
+    flex: 3,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  textListEdit: {
+    color: 'blue',
+    marginRight: 20
+  },
+  textListDelete: {
+    color: 'red'
+  },
+
+
+  navView: {
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: 'red',
+    borderBottomWidth: 2,
+    borderBottomColor: 'white',
+  },
+  buttonNavigation: {
+    backgroundColor: 'yellow',
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    marginHorizontal: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'black',
+    fontWeight: 'bold',
+  },
+
+  titlePageView: {
+    flex: 5,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    marginRight: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'white',
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+})
---
+++ Home.js
@@ -0,0 +1,644 @@
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  useWindowDimensions,
+  Text,
+} from 'react-native';
+
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  withRepeat,
+} from 'react-native-reanimated';
+import * as Speech from 'expo-speech';
+
+
+const fetchPokemon = async (id) => {
+  const responsePokemon = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${id}`
+  );
+
+  const pokemon = await responsePokemon.json();
+
+  const responseDetails = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}`
+  );
+  const pokemonDetails = await responseDetails.json();
+
+  const name = pokemonDetails.name;
+
+  const description =
+    pokemonDetails.flavor_text_entries[0]
+      .flavor_text;
+
+  const image =
+    pokemon.sprites.other['official-artwork']
+      .front_default;
+
+  return { name, description, image };
+};
+
+
+function Home({ navigation }) {
+  const dimensions = useWindowDimensions();
+
+  const translation = useSharedValue(0);
+  const overlayOpacity = useSharedValue(1);
+  const lightOpacity = useSharedValue(1);
+
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    description: '',
+    image: null,
+  });
+
+  const updatePokemon = async () => {
+    const pokemonID =
+      Math.floor(Math.random() * 150) + 1;
+
+    const pokemon = await fetchPokemon(pokemonID);
+
+    setPokemon(pokemon);
+
+  };
+
+  const searchPokemon = async () => {
+
+    const pokemon = await fetchPokemon(text);
+
+    setPokemon(pokemon);
+
+  };
+
+  useEffect(() => {
+    updatePokemon();
+  }, []);
+
+  const startFlashingLight = () => {
+    lightOpacity.value = withRepeat(
+      withTiming(0.5, { duration: 250 }),
+      -1,
+      true
+    );
+  };
+
+  const stopFlashingLight = () => {
+    lightOpacity.value = withTiming(1);
+  };
+
+  const sayPokemonName = () => {
+    Speech.speak(
+      `${pokemon.name}: ${pokemon.description}`,
+      {
+        language: 'en-US',
+        pitch: 0.9,
+        rate: 1.0,
+        onStart: () => {
+          overlayOpacity.value = withTiming(0, {
+            duration: 500,
+          });
+
+          startFlashingLight();
+        },
+        onDone: stopFlashingLight,
+        onStopped: stopFlashingLight,
+      }
+    );
+  };
+
+  const toggleCover = () => {
+    if (translation.value === 0) {
+      // Open
+      translation.value = withTiming(
+        dimensions.width,
+        { duration: 500 },
+        () => {
+          runOnJS(sayPokemonName)();
+        }
+      );
+    } else {
+      // Close
+      translation.value = withTiming(
+        0,
+        {
+          duration: 500,
+        },
+        () => {
+          overlayOpacity.value = 1;
+          runOnJS(updatePokemon)();
+        }
+      );
+
+      Speech.stop();
+    }
+  };
+
+  const searchResult = () => {
+
+    // Close
+    translation.value = withTiming(
+      0,
+      {
+        duration: 500,
+      },
+      () => {
+        overlayOpacity.value = 1;
+        runOnJS(searchPokemon)();
+      }
+    );
+    Speech.stop();
+
+    // Open
+    translation.value = withTiming(
+      dimensions.width,
+      { duration: 500 },
+      () => {
+        runOnJS(sayPokemonName)();
+      }
+
+
+    );
+
+
+  };
+
+  const coverStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translation.value },
+      ],
+    };
+  });
+
+  const overlayStyle = useAnimatedStyle(() => {
+    return {
+      opacity: overlayOpacity.value,
+    };
+  });
+
+  const lightStyle = useAnimatedStyle(() => {
+    return {
+      opacity: lightOpacity.value,
+    };
+  });
+
+
+
+  const url = "http://localhost/api/api.php";
+
+  const saveToFavorite = () => {
+
+    var urlAksi = url + "/?op=create";
+
+    fetch(urlAksi, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: "nama=" + pokemon.name
+    })
+      .then((response) => response.json())
+
+  }
+
+  const [text, setText] = useState('raichu');
+
+  const goToFavorite = () => {
+    navigation.navigate('favorit');
+    Speech.stop();
+  }
+
+  return (
+    <>
+      <StatusBar hidden />
+
+      <Animated.View
+        pointerEvents="none"
+        style={[styles.cover, coverStyle]}
+      >
+        <View style={styles.coverHandle} />
+        <View style={styles.coverBinding} />
+      </Animated.View>
+
+
+      <View style={styles.pokedex}>
+        <SafeAreaView>
+          <View style={styles.lights}>
+            <View style={styles.mainLight}>
+              <Animated.View
+                style={[
+                  styles.mainLightInner,
+                  lightStyle,
+                ]}
+              />
+            </View>
+
+            <View style={styles.redLight} />
+            <View style={styles.orangeLight} />
+            <View style={styles.greenLight} />
+
+          </View>
+
+          <View style={{ flexDirection: 'row' }}>
+
+            <View style={styles.searchView}>
+              <TextInput
+                style={{ height: 40, paddingLeft: 5, flex: 3, backgroundColor: 'white' }}
+                placeholder="Type to search"
+                onChangeText={
+                  text => {
+                    setText(text)
+                  }
+                }
+
+              />
+
+              <TouchableOpacity
+                style={styles.buttonSearch}
+                onPress={searchResult}
+              >
+                <Text>Seach</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              style={styles.buttonNavigation}
+              onPress={goToFavorite}
+            >
+              <Text>My Favorit</Text>
+            </TouchableOpacity>
+          </View>
+
+
+          <Pressable
+            onPress={toggleCover}
+            style={styles.pokedexTouchable}
+          >
+
+            <View style={styles.details}>
+              <View style={styles.screen}>
+                <View style={styles.screenHeader}>
+                  <View
+                    style={
+                      styles.screenHeaderHole
+                    }
+                  />
+
+                  <View
+                    style={
+                      styles.screenHeaderHole
+                    }
+                  />
+                </View>
+
+                <View
+                  style={styles.screenPokemon}
+                >
+                  <Animated.View
+                    style={[
+                      styles.screenPokemonOverlay,
+                      overlayStyle,
+                    ]}
+                  />
+
+                  <Image
+                    source={{
+                      uri: pokemon.image,
+                    }}
+                    style={
+                      styles.screenPokemonImage
+                    }
+                  />
+                </View>
+
+                <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
+
+                  <TouchableOpacity
+                    style={styles.buttonAddFavorit}
+                    onPress={saveToFavorite} >
+                    <Text>+ Favorit</Text>
+                  </TouchableOpacity>
+
+                  <View
+                    style={styles.screenSoundHoles}
+                  >
+                    <View
+                      style={[
+                        styles.screenSoundHole,
+                        styles.screenSoundHoleLarge,
+                      ]}
+                    />
+
+                    <View
+                      style={[
+                        styles.screenSoundHole,
+                        styles.screenSoundHoleSmall,
+                      ]}
+                    />
+
+                    <View
+                      style={[
+                        styles.screenSoundHole,
+                        styles.screenSoundHoleMedium,
+                      ]}
+
+                    />
+
+                    <View
+                      style={[
+                        styles.screenSoundHole,
+                        styles.screenSoundHoleLarge,
+                      ]}
+                    />
+                  </View>
+                </View>
+
+
+              </View>
+
+              <View style={styles.detailView}>
+                <View style={styles.pokeName}>{pokemon.name}</View>
+                <View style={styles.pokeDesc}>{pokemon.description} </View>
+              </View>
+
+            </View>
+
+          </Pressable>
+        </SafeAreaView>
+      </View>
+
+    </>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  cover: {
+    ...StyleSheet.absoluteFillObject,
+    top: 155,
+    zIndex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderTopWidth: 2,
+    borderTopColor: 'white',
+  },
+
+  coverHandle: {
+    height: 40,
+    width: 40,
+    // Orange
+    backgroundColor: 'rgb(255, 165, 0)',
+    borderWidth: 2,
+    // Dark orange
+    borderColor: 'black',
+    transform: [
+      { rotate: '45deg' },
+      { translateX: -30 },
+    ],
+  },
+  coverBinding: {
+    height: '100%',
+    width: 50,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    borderLeftWidth: 1,
+    borderLeftColor: 'rgb(15, 15, 15)',
+  },
+  pokedexTouchable: {
+    flex: 1,
+  },
+  pokedex: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  lights: {
+    flexDirection: 'row',
+    borderBottomWidth: 2,
+    borderBottomColor: 'white',
+    backgroundColor: 'red',
+    paddingBottom: 15,
+  },
+  mainLight: {
+    marginLeft: 20,
+    height: 80,
+    width: 80,
+    // Light blue
+    backgroundColor: 'rgb(75, 154, 244)',
+    borderRadius: 40,
+    borderWidth: 5,
+    borderColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainLightInner: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    // Dark blue
+    backgroundColor: 'rgb(20, 90, 170)',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowRadius: 20,
+    shadowOpacity: 0.9,
+  },
+  redLight: {
+    marginLeft: 15,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    // Red
+    backgroundColor: 'rgb(255, 0, 0)',
+    borderWidth: 2,
+    // Dark red
+    borderColor: 'rgb(200, 0, 0)',
+  },
+  orangeLight: {
+    marginLeft: 10,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    // Orange
+    backgroundColor: 'rgb(255, 165, 0)',
+    borderWidth: 2,
+    // Dark orange
+    borderColor: 'rgb(200, 165, 0)',
+  },
+  greenLight: {
+    marginLeft: 10,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    // Green
+    backgroundColor: 'rgb(0, 150, 0)',
+    borderWidth: 2,
+    // Dark green
+    borderColor: 'rgb(0, 100, 0)',
+  },
+
+  searchView: {
+    flex: 3,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    marginTop: 5,
+    marginLeft: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+
+  buttonSearch: {
+    alignItems: "center",
+    backgroundColor: "grey",
+    padding: 10,
+    flex: 1,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    fontWeight: 'bold',
+  },
+
+  buttonNavigation: {
+    backgroundColor: 'yellow',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+    marginHorizontal: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'black',
+    fontWeight: 'bold',
+  },
+
+
+  details: {
+    flexDirection: 'column',
+    height: '100%',
+  },
+  screen: {
+    flex: 1,
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+    marginVertical: 30,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  screenHeader: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 35,
+  },
+  screenHeaderHole: {
+    backgroundColor: 'rgb(200, 0, 0)',
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+    marginHorizontal: 10,
+  },
+  screenPokemon: {
+    marginHorizontal: 30,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  screenPokemonOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 1,
+  },
+  screenPokemonImage: {
+    height: 150,
+    width: 150,
+  },
+  screenSoundHoles: {
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    height: 35,
+    marginRight: 30,
+  },
+  screenSoundHole: {
+    height: 2,
+    width: 40,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    marginBottom: 2,
+  },
+  screenSoundHoleLarge: {
+    width: 40,
+  },
+  screenSoundHoleMedium: {
+    width: 38,
+  },
+  screenSoundHoleSmall: {
+    width: 35,
+  },
+
+  buttonAddFavorit: {
+    width: 70,
+    height: 20,
+    backgroundColor: 'yellow',
+    margin: 10,
+    marginRight: 30,
+  },
+
+  detailView: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+
+  pokeName: {
+    flex: 1,
+    margin: 20,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    fontSize: 30,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'black',
+    color: 'white',
+  },
+
+  pokeDesc: {
+    flex: 2,
+    margin: 20,
+    backgroundColor: 'green',
+    height: 120,
+    fontSize: 20,
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 5,
+    color: 'white',
+  },
+
+});
